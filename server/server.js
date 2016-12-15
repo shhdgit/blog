c180e267193b052707292c98fbedaf2ee8c7b786
suhaha <jklopsdfw@gmail.com>
2016-12-16 04:22:32 +0800

    Path Change

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ require('dotenv').config()
 const Koa = require('koa')
 const router = require('./http/routes')
 const serve = require('koa-static')
+const path = require('path')
 
 const port = process.env.PORT || 3000
 const app = new Koa()
 
-app.use(serve('../client/front'))
+app.use(serve(path.resolve(process.cwd(), '../client/front')))
 app.use(router.routes())
 
 app.listen(port)
