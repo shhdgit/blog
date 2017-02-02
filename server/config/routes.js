75fb20d5fc4d98098e662b410aeb4bd2d50af394
suhaha <jklopsdfw@gmail.com>
2017-02-02 19:40:42 +0800

    test

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,15 +8,15 @@ const AuthMiddleware = require('../middleware/authorize')
 
 // login
 router
-  .get('/login', User.login)
-  .post('/login', User.login)
-  .get('/test', AuthMiddleware.auth, User.test)
+  .get('/**', User.login)
+//   .post('/login', User.login)
+//   .get('/test', AuthMiddleware.auth, User.test)
 
-// article
-  .get('/article', AuthMiddleware.auth, Article.get_list)
-  .post('/article', AuthMiddleware.auth, Article.save)
-  .get('/article/:id', AuthMiddleware.auth, Article.get)
-  .post('/article/:id', AuthMiddleware.auth, Article.update)
-  .del('/article', AuthMiddleware.auth, Article.del, Article.get_list)
+// // article
+//   .get('/article', AuthMiddleware.auth, Article.get_list)
+//   .post('/article', AuthMiddleware.auth, Article.save)
+//   .get('/article/:id', AuthMiddleware.auth, Article.get)
+//   .post('/article/:id', AuthMiddleware.auth, Article.update)
+//   .del('/article', AuthMiddleware.auth, Article.del, Article.get_list)
 
 module.exports = router
