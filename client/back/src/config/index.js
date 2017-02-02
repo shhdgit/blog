0f163d60b81843a6e8dacd8e3fb01c2cd29055d8
suhaha <jklopsdfw@gmail.com>
2017-02-02 18:22:05 +0800

    Back-end api

--- client/back/src/config/index.js
+++ client/back/src/config/index.js
@@ -1,4 +1,4 @@
 export default {
-  apiURL: (process.env.NODE_ENV === 'production') ? 'http://rollaround.cn:3000' : '/api',
+  apiURL: (process.env.NODE_ENV === 'production') ? '/api' : '/api',
   requestTimeout: 30000
 }
