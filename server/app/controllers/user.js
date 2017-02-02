480d1e633efd718b98c9b5cb6273ba2ed86a593b
suhaha <jklopsdfw@gmail.com>
2017-02-02 19:24:11 +0800

    Back-end api

--- server/app/controllers/user.js
+++ server/app/controllers/user.js
@@ -2,28 +2,29 @@ const mongoose = require('mongoose')
 const User = require('../models/user')
 
 exports.login = async function (ctx, next) {
-  const _user = ctx.request.body
-  const name = _user.name
-  const pwd = _user.pwd
-  const user = await User.findOne({ name })
+  // const _user = ctx.request.body
+  // const name = _user.name
+  // const pwd = _user.pwd
+  // const user = await User.findOne({ name })
 
-  if (user) {
-    const isMatch = await user.comparePwd(pwd)
+  // if (user) {
+  //   const isMatch = await user.comparePwd(pwd)
 
-    if (isMatch) {
-      ctx.session.user = {
-        name: user.name
-      }
-      ctx.body = {
-        success: 1,
-        msg: 'success'
-      }
-    } else {
-      ctx.status = 401
-    }
-  } else {
-    ctx.status = 400
-  }
+  //   if (isMatch) {
+  //     ctx.session.user = {
+  //       name: user.name
+  //     }
+  //     ctx.body = {
+  //       success: 1,
+  //       msg: 'success'
+  //     }
+  //   } else {
+  //     ctx.status = 401
+  //   }
+  // } else {
+  //   ctx.status = 400
+  // }
+  ctx.body = ctx.request
 
   next()
 }
